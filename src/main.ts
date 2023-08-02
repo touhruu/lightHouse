48b17bde7beb2427ffac4e980590f8ee2796a463
touhruu <vika4ka.karelina@gmail.com>
2023-08-02 13:49:25 +0300

    added dialog window for add board

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import App from './App.vue';
 import { router } from './router';
 import { store } from './store';
 import PrimeVue from 'primevue/config';
-import ToastService from 'primevue/toastservice';
-import Menu from 'primevue/menu';
+import Dialog from 'primevue/dialog';
+import ColorPicker from 'primevue/colorpicker';
+import Checkbox from 'primevue/checkbox';
 
 createApp(App)
 .use(store)
 .use(router)
 .use(PrimeVue)
-.use(ToastService)
-.component('Menu', Menu)
+.component('Dialog', Dialog)
+.component('ColorPicker', ColorPicker)
+.component('Checkbox', Checkbox)
 .mount('#app')
--- src/store/types.ts
+++ src/store/types.ts
@@ -2,3 +2,14 @@ export interface IMenuUser{
     id: number;
     name: string;
 }
+
+export interface IGradient{
+    id: number;
+    color: string;
+}
+
+export interface IBoard{
+    title: string,
+    color: string,
+    animation: boolean
+}
